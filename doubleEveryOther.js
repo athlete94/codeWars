4810b41b777424fc93288d1fdc9b03e09a82630a
Azamat <azamt14@gmail.com>
2021-12-22 20:35:53 +0300

    double every second element

--- doubleEveryOther.js
+++ doubleEveryOther.js
@@ -2,7 +2,7 @@ function doubleEveryOther(a) {
     for (let i = 1; i < a.length; i += 2) {
       a[i] = a[i]*2
     }
-    return a;
+    return a;  
   }
 
   console.log(doubleEveryOther([1,2,3,4]))
