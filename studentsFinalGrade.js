24995e18ab41cb856c295b2876a05054555319a4
Azamat <azamt14@gmail.com>
2022-01-05 16:48:29 +0300

    function calculate final grade

---
+++ studentsFinalGrade.js
@@ -0,0 +1,11 @@
+function finalGrade (exam, projects) {
+    if(exam > 90 && projects > 10) {
+      return 100
+    }
+    else if (exam > 75 && projects > 4) {
+      return 90
+    }
+    else if( exam > 50 && projects > 1) {
+      return 75
+    }
+  }
